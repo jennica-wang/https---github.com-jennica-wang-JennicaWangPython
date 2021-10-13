dc36396797132cefb72be7db53b70d6acf0293aa
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-10-14 10:55:39 +1100

    Finished

---
+++ 01HelloWorld.py
@@ -0,0 +1,8 @@
+print(" -"+"-"*47)
+print("|                                                |")
+print("|    01HelloWorld                                |")
+print("|    Name : Jennica Wang                         |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" -"+"-"*47)
+print("Hello World")
